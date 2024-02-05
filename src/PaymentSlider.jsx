2035f999fe56bf3f6ae16295d7726340ff991653
Kushal <kushsonkamble@gmail.com>
2024-02-05 23:31:26 +0530

    checking payment slider

--- src/PaymentSlider.jsx
+++ src/PaymentSlider.jsx
@@ -8,12 +8,12 @@ import pay2 from '../images/payment2.jpg'
 import pay3 from '../images/payment3.jpg'
 import pay4 from '../images/payment4.jpg'
 import pay5 from '../images/payment5.jpg'
-import pay6 from '../images/payment6.jpg'
-import pay7 from '../images/payment7.jpg'
+// import pay6 from '../images/payment6.jpg'
+// import pay7 from '../images/payment7.jpg'
 
 // PAYMENT SILDER COMPONENT
 export default function Paymentslider() {
-    const images = [pay1, pay2, pay3, pay4, pay5, pay6, pay7];
+    const images = [pay1, pay2, pay3, pay4, pay5];
     const p1 = 0;
     const p2 = 1;
 
@@ -25,8 +25,6 @@ export default function Paymentslider() {
                 <img src={pay3} alt="" />
                 <img src={pay4} alt="" />
                 <img src={pay5} alt="" />
-                <img src={pay6} alt="" />
-                <img src={pay7} alt="" />
             </div>
         </div>
     )
